6fbc21e91cbca472dd899fa12f0a2233d7f7bf6e
Victor Li <vli8@fordham.edu>
2018-06-11 12:30:00 -0400

    first part

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr){
+  let isSwapped = false;
+  while(!isSwapped)
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i]> arr[i+1]){
+        const temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+      }
+      else{
+        continue;
+      }
+    }
+  return arr;
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,14 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+  it('handles one item', function(){
+    expect(bubbleSort([1])).toEqual([1]);
+  })
+  it('handles multiple items',function(){
+    expect(bubbleSort([3,2,1]).toEqual([1,2,3]));
+  })
+  it('handles a complicated array',function(){
+    expect(bubbleSort([56,40,82,12,100]).toEqual([12,40,56,82,100]));
+  })
+});
